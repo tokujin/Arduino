10791bd83f4ad89e0a2203d559da14c2e0fafb92
tokujin <norihito.yuki@me.com>
2013-04-21 20:15:55 -0400

    change numberOfServos, start serial communication
    change numberOfServos, start serial communication

--- Arduino-sketch/MS2Final/servos2013/servos2013.ino
+++ Arduino-sketch/MS2Final/servos2013/servos2013.ino
@@ -1,41 +1,41 @@
-/*
-    This sketch sweeps a servo on channel 0.
-
-    To connect a servo:
-     1. Put a 2k-5k pull-up resistor (R0 below; I've tried with 3.3k) between the
-          servo control output pin and +5v.
-     2. Connect that same pin to the servo's control line like so
-
-        servo data pin
-              |     _____
-     OUTn ----+----[_____]---+5v
-                      R0
-
-    Steve Pomeroy <steve ~AT~ staticfree.info>, 2009-01-20 */
+//From the bildr article: http://bildr.org/2012/03/servos-tlc5940-arduino
+//Requires the Tlc5940 library. http://code.google.com/p/tlc5940arduino/downloads/list
 
 #include "Tlc5940.h"
 #include "tlc_servos.h"
 
-#define SERVO_CHANNEL   0
-#define DELAY_TIME      20
-#define NUM_TLCS  7 // 7 daisy chain-nori
+int numberOfServos = 9; //how many servos on the chain?
 
-void setup()
-{
+
+void setup(){
   tlc_initServos();  // Note: this will drop the PWM freqency down to 50Hz.
 }
 
-void loop()
-{
-  for (int angle = 0; angle < 180; angle++) {
-    tlc_setServo(SERVO_CHANNEL, angle);
-    Tlc.update();
-    delay(DELAY_TIME);
-  }
-  for (int angle = 180; angle >= 0; angle--) {
-    tlc_setServo(SERVO_CHANNEL, angle);
-    Tlc.update();
-    delay(DELAY_TIME);
-  }
-}
+void loop(){
+  //loop through all the servos and move one at a time to 180º
+  for(int i = 0; i<numberOfServos; i++){
 
+    for (int angle = 0; angle < 180; angle+= 10) {
+      tlc_setServo(i, angle);
+      Tlc.update();
+      delay(20);
+    }
+
+    delay(200);
+  }
+
+  //loop through all the servos and move one at a time to 0º
+  for(int i = 0; i<numberOfServos; i++){
+
+    for (int angle = 180; angle >= 0; angle-= 10) {
+      tlc_setServo(i, angle);
+      Tlc.update();
+      delay(20);
+    }
+
+    delay(200);
+  }
+
+
+
+}
